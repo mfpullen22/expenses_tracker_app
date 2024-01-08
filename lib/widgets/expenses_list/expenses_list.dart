f3c9a3a7419eef725f1b3d612bb7acb772b152f5
Matthew Pullen <matthew.pullen@gmail.com>
2024-01-08 00:18:33 -0600

    Expense list and date formatted

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -1,4 +1,5 @@
 import "package:expense_tracker_app/model/expense.dart";
+import "package:expense_tracker_app/widgets/expenses_list/expense_item.dart";
 import "package:flutter/material.dart";
 
 class ExpensesList extends StatelessWidget {
@@ -10,6 +11,6 @@ class ExpensesList extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
         itemCount: expenses.length,
-        itemBuilder: (ctz, index) => Text(expenses[index].title));
+        itemBuilder: (ctz, index) => ExpenseItem(expenses[index]));
   }
 }
